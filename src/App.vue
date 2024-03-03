8d4b59949fb6ef79422d1d5d71ae5f143238e08d
Girish <gspande7@gmail.com>
2024-03-03 17:07:09 -0500

    GET request for lists works and task lists showing on board

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { ref } from 'vue'
+import TaskBoard from './components/TaskBoard.vue'
 </script>
 
 <template>
   <div>
     <h1>Task Management App</h1>
+    <TaskBoard />
   </div>
 </template>
 
